02cee6436d02466a4e0ac8385fcae8229085b288
17app001 <17app001@gmail.com>
2026-03-03 16:46:26 +0800

    移除舊版專案

---
+++ Stackdose.App.ModelE.SinglePage/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Stackdose.UI.Templates.Helpers;
+using Stackdose.UI.Core.Helpers;
+using Stackdose.UI.Core.Models;
+using System.Windows;
+
+namespace Stackdose.App.ModelE.SinglePage;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        AppThemeBootstrapper.Apply(this);
+        SecurityContext.QuickLogin(AccessLevel.SuperAdmin);
+        base.OnStartup(e);
+
+        var mainWindow = new MainWindow();
+        MainWindow = mainWindow;
+        mainWindow.Show();
+    }
+}
